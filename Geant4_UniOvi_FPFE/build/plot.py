35ae69f368b96bbc9e31823de186239dfa8a5fc4
Victor <victorhca93@gmail.com>
2021-05-13 01:58:36 +0200

    sixth commit

--- Geant4_UniOvi_FPFE/build/plot.py
+++ Geant4_UniOvi_FPFE/build/plot.py
@@ -11,22 +11,21 @@ t = f.Get("histograms")
 edep = t.Get("Edep")
 trackL = t.Get("trackL")
 
-
 # -----------------------------------------------------
 # Creamos el Canvas
-c = r.TCanvas("c", "Si_prot_12GeV_5.6um", 20, 20, 1000, 1000)
+c = r.TCanvas("c", "Si_elec_100MeV_5.6um", 20, 20, 1000, 1000)
 
 c.Divide(1, 2) 
 
 c.cd(1)
 edep.Draw("hist")
-edep.SaveAs("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/proton/Si_prot_12GeV_5.6um_edep.xlsx")
+edep.SaveAs("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/electron/Si_elec_100MeV_5.6um_1_edep.xlsx")
 
 c.cd(2)
 trackL.Draw("hist")
-edep.SaveAs("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/proton/Si_prot_12GeV_5.6um_trackL.xlsx")
+edep.SaveAs("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/electron/Si_elec_100MeV_5.6um_1_trackL.xlsx")
 
-c.Print("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/proton/Si_prot_12GeV_5.6um.png", "png")
+c.Print("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/electron/Si_elec_100MeV_5.6um_1.png", "png")
 
-out = r.TFile("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/proton/Si_prot_12GeV_5.6um.root", "recreate")
+out = r.TFile("/nfs/fanae/user/vichuca93/g4work/G4_VHCA_FPFE_UniOvi/Geant4_UniOvi_FPFE/histogramas/electron/Si_elec_100MeV_5.6um_1.root", "recreate")
 c.Write()
